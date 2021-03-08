0d81d8261387a46e87790a9b6b7a67aa7acb2b98
YunWu <vv_face@hotmail.com>
2021-03-08 12:28:14 +0000

    add files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#5 SMP Wed Feb 17 22:21:10 GMT 2021"
+#define LINUX_COMPILE_BY "udrc"
+#define LINUX_COMPILE_HOST "udrc-Alienware-m15"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04)"
